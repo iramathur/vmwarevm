94d929af17e43e41c71fcd72db93f85dc87d3a6b
itom-qe <50704200+itom-qe@users.noreply.github.com>
2020-12-18 01:43:00 +0530

    Update simplevm.tf

--- simplevm.tf
+++ simplevm.tf
@@ -1,13 +1,10 @@
-provider "vsphere" {
+provider "vsphere" {  
   user           = "cmpqa.svc@itomcmp.servicenow.com"
   password       = "snc!23$"
   vsphere_server = "10.198.1.13"
 
   # If you have a self-signed cert
   allow_unverified_ssl = true
-}
-resource "random_id" "server" {
-  byte_length = 9
 }
 
 data "vsphere_datacenter" "dc" {
@@ -24,33 +21,26 @@ data "vsphere_resource_pool" "pool" {
  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
-data "vsphere_virtual_machine" "template" {
-  name          = "rhel8"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
-}
-
 data "vsphere_network" "network" {
   name          = "portGroup-1004"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 resource "vsphere_virtual_machine" "vm" {
-  name             = "test-checking-test-vm-code${random_id.server.hex}"
-  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+  name             = "terraform-test"
+  resource_pool_id     = "${data.vsphere_resource_pool.pool.id}"
   datastore_id     = "${data.vsphere_datastore.datastore.id}"
 
-  num_cpus = 1
-  memory   = 512
-  guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
+  num_cpus = 2
+  memory   = 1024
+  guest_id = "centos7"
 
   network_interface {
     network_id = "${data.vsphere_network.network.id}"
   }
 
-wait_for_guest_net_timeout = 0
-
   disk {
     label = "disk0"
-    size  = 4
+    size  = 20
   }
 }
