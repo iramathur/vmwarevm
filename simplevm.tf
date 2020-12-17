07ca0ee45c38212cbe690893d9ec24725e2ba51a
itom-qe <50704200+itom-qe@users.noreply.github.com>
2020-12-18 02:04:28 +0530

    Update simplevm.tf

--- simplevm.tf
+++ simplevm.tf
@@ -30,6 +30,7 @@ resource "vsphere_virtual_machine" "vm" {
   name             = "terraform-test"
   resource_pool_id     = "${data.vsphere_resource_pool.pool.id}"
   datastore_id     = "${data.vsphere_datastore.datastore.id}"
+  wait_for_guest_net_timeout = -1
 
   num_cpus = 2
   memory   = 1024
