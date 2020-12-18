700fbe15b76d356be2d670990f7bbe197a506633
itom-qe <50704200+itom-qe@users.noreply.github.com>
2020-12-18 13:33:44 +0530

    Update simplevm.tf

--- simplevm.tf
+++ simplevm.tf
@@ -2,7 +2,7 @@ provider "vsphere" {
   user           = "cmpqa.svc@itomcmp.servicenow.com"
   password       = "snc!23$"
   vsphere_server = "10.198.1.13"
-  version = "< 1.16.0"
+  version = "~> 1.16.0"
 
   # If you have a self-signed cert
   allow_unverified_ssl = true
