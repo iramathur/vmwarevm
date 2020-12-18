5df5aa1dd9affab785a6d7f8b9647d7fb672a290
itom-qe <50704200+itom-qe@users.noreply.github.com>
2020-12-18 10:25:04 +0530

    Update simplevm.tf

--- simplevm.tf
+++ simplevm.tf
@@ -1,3 +1,6 @@
+# Basic configuration withour variables
+
+# Define authentification configuration
 provider "vsphere" {  
   user           = "cmpqa.svc@itomcmp.servicenow.com"
   password       = "snc!23$"
@@ -7,41 +10,72 @@ provider "vsphere" {
   allow_unverified_ssl = true
 }
 
+#### RETRIEVE DATA INFORMATION ON VCENTER ####
+
 data "vsphere_datacenter" "dc" {
   name = "devcloud"
 }
 
+data "vsphere_resource_pool" "pool" {
+  # If you haven't resource pool, put "Resources" after cluster name
+  name          = "fenrir/Resources"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+# Retrieve datastore information on vsphere
 data "vsphere_datastore" "datastore" {
   name          = "vmstore"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_resource_pool" "pool" {
-  name          = "fenrir/Resources"
- datacenter_id = "${data.vsphere_datacenter.dc.id}"
-}
-
+# Retrieve network information on vsphere
 data "vsphere_network" "network" {
   name          = "portGroup-1004"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-resource "vsphere_virtual_machine" "vm" {
-  name             = "terraform-test"
-  resource_pool_id     = "${data.vsphere_resource_pool.pool.id}"
-  datastore_id     = "${data.vsphere_datastore.datastore.id}"
-  wait_for_guest_net_timeout = -1
+# Retrieve template information on vsphere
+data "vsphere_virtual_machine" "template" {
+  name          = "centos7_64Guest"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
 
-  num_cpus = 2
-  memory   = 1024
-  guest_id = "centos7_64Guest"
+#### VM CREATION ####
 
+# Set vm parameters
+resource "vsphere_virtual_machine" "vm-one" {
+  name             = "vm-one"
+  num_cpus         = 2
+  memory           = 4096
+  datastore_id     = data.vsphere_datastore.datastore.id
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  guest_id         = data.vsphere_virtual_machine.template.guest_id
+  scsi_type        = data.vsphere_virtual_machine.template.scsi_type
+
+  # Set network parameters
   network_interface {
-    network_id = "${data.vsphere_network.network.id}"
+    network_id = data.vsphere_network.network.id
   }
 
+  # Use a predefined vmware template has main disk
   disk {
-    label = "disk0"
-    size  = 20
+    name = "vm-one.vmdk"
+    size = "30"
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+
+    customize {
+      linux_options {
+        host_name = "vm-one"
+        domain    = "test.internal"
+      }
+
+      network_interface {
+      }
+
+      ipv4_gateway = "10.198.4.1"
+    }
   }
 }
