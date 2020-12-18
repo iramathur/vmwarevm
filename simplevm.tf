d31b31820de48802018337955beeef1fb4167f07
itom-qe <50704200+itom-qe@users.noreply.github.com>
2020-12-18 10:56:29 +0530

    Update simplevm.tf

--- simplevm.tf
+++ simplevm.tf
@@ -1,3 +1,12 @@
+provider "vsphere" {
+  user           = "cmpqa.svc@itomcmp.servicenow.com"
+  password       = "snc!23$"
+  vsphere_server = "10.198.1.13"
+
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
 data "vsphere_datacenter" "dc" {
   name = "dc1"
 }
