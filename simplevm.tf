53dc733d5893ae3f1be6817699af4216adfe376f
itom-qe <50704200+itom-qe@users.noreply.github.com>
2020-12-18 22:37:41 +0530

    Update simplevm.tf

--- simplevm.tf
+++ simplevm.tf
@@ -2,71 +2,66 @@ provider "vsphere" {
   user           = "cmpqa.svc@itomcmp.servicenow.com"
   password       = "snc!23$"
   vsphere_server = "10.198.1.13"
-  version = "~> 1.16.0"
-
   # If you have a self-signed cert
   allow_unverified_ssl = true
 }
-resource "random_id" "server" {
-  byte_length = 8
-}
-
-
+#### RETRIEVE DATA INFORMATION ON VCENTER ####
 data "vsphere_datacenter" "dc" {
   name = "devcloud"
 }
-
-resource "vsphere_folder" "foldervm" {
-  path          = "terrtar-testng${random_id.server.hex}"
-  type          = "vm"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
-}
-
-
-data "vsphere_datastore" "datastore" {
-  name          = "vmstore"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
-}
-
 data "vsphere_resource_pool" "pool" {
   name          = "fenrir/Resources"
  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
-
-data "vsphere_virtual_machine" "template" {
-  name          = "ubuntu16"
+# Retrieve datastore information on vsphere
+data "vsphere_datastore" "datastore" {
+  name          = "vmstore"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
-data "vsphere_tag_category" "category" {
-  name = "Dev_lab"
-}
-data "vsphere_tag" "tag" {
-  name        = "Dev_app"
-  category_id = "${data.vsphere_tag_category.category.id}"
-}
+# Retrieve network information on vsphere
 data "vsphere_network" "network" {
   name          = "portGroup-1004"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
-
-resource "vsphere_virtual_machine" "vm" {
-  name             = "trial-vm${random_id.server.hex}"
-  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
-  datastore_id     = "${data.vsphere_datastore.datastore.id}"
-  folder = "${vsphere_folder.foldervm.path}"
-  tags = ["${data.vsphere_tag.tag.id}"]
-  num_cpus = 1
-  memory   = 512
-  guest_id = "centos64Guest"
-
+# Retrieve template information on vsphere
+data "vsphere_virtual_machine" "template" {
+  name          = "ubuntu16"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+#### VM CREATION ####
+# Set vm parameters
+resource "vsphere_virtual_machine" "vm-one" {
+  name                 = "icheck"
+  num_cpus             = 2
+  memory               = 4096
+  datastore_id         = "${data.vsphere_datastore.datastore.id}"
+  #host_system_id       = "${data.vsphere_host.host.id}"
+  resource_pool_id     = "${data.vsphere_resource_pool.pool.id}"
+  guest_id             = "${data.vsphere_virtual_machine.template.guest_id}"
+  scsi_type            = "${data.vsphere_virtual_machine.template.scsi_type}"
+  # Set network parameters
   network_interface {
-    network_id = "${data.vsphere_network.network.id}"
+    network_id         = "${data.vsphere_network.network.id}"
   }
-
-  wait_for_guest_net_timeout = 0
-
+  # Use a predefined vmware template has main disk
   disk {
-    label = "disk0"
-    size  = 4
+    label = "vm-two.vmdk"
+    size = "30"
+    thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
+  }
+  clone {
+    template_uuid = "${data.vsphere_virtual_machine.template.id}"
+    customize {
+      linux_options {
+        host_name = "vm-two"
+        domain    = "test.internal"
+      }
+      network_interface {
+        ipv4_address    = "10.198.4.157"
+        ipv4_netmask    = 24
+        dns_server_list = ["10.198.4.10"]
+      }
+      ipv4_gateway = "10.198.4.1"
+    }
   }
 }
